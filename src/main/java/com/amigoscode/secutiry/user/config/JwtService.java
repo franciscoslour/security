5c531006d31c28ba88da84b832dd73d2ac0cc98c
Francisco S. Lourenço <franciscolour2011@gmail.com>
2024-06-24 14:22:47 +0100

    refactor: filtro JWT

---
+++ src/main/java/com/amigoscode/secutiry/user/config/JwtService.java
@@ -0,0 +1,71 @@
+package com.amigoscode.secutiry.user.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECURITY_KEY = "183AB864DF2CA1898744DCEB59A48";
+
+
+    public<T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+               .setClaims(extraClaims)
+               .setSubject(userDetails.getUsername())
+               .setIssuedAt(new Date(System.currentTimeMillis()))
+               .setExpiration(new Date(System.currentTimeMillis() + 1000 *60*24))
+               .signWith(SignatureAlgorithm.HS256,getSigningKey())
+               .compact();
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username =extractUsername(token);
+        return (username.equalsIgnoreCase(userDetails.getUsername())) && ! isTokenExperide(token);
+    }
+
+    private boolean isTokenExperide(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private byte[] getSigningKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECURITY_KEY);
+        return Keys.hmacShaKeyFor(keyBytes).getEncoded();
+    }
+
+}
